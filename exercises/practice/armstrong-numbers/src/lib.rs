d6db3a5fed0a5ee58758abd999cee938c15ddf72
inssa.gwajang <inssa.gwajang@gmail.com>
2022-03-15 15:51:01 +0900

    conitnue to solve

--- exercises/practice/armstrong-numbers/src/lib.rs
+++ exercises/practice/armstrong-numbers/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let s = num.to_string();
+    let l = s.len() as u32;
+
+    num == s
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .map(|n| n.pow(l))
+        .sum()
+
+    // num == s.chars().fold(0, |mut r, c| {
+    //     r += c.to_digit(10).unwrap().pow(l);
+    //     r
+    // })
 }
--- exercises/practice/gigasecond/src/lib.rs
+++ exercises/practice/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
+use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
-// Returns a DateTime one billion seconds after start.
+const GIGASECOND: i64 = 1_000_000_000;
+
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    start + Duration::seconds(GIGASECOND)
 }
