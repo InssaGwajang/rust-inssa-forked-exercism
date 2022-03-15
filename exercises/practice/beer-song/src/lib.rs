bae8595e353c40483ed9a623c993444833943149
inssa.gwajang <inssa.gwajang@gmail.com>
2022-03-15 20:47:39 +0900

    continue to explore

--- exercises/practice/beer-song/src/lib.rs
+++ exercises/practice/beer-song/src/lib.rs
@@ -1,7 +1,39 @@
+fn bottles(n: u32) -> &'static str {
+    match n {
+        1 => "bottle",
+        _ => "bottles",
+    }
+}
+
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    format!(
+        "{}\n{}\n",
+        match n {
+            0 => String::from("No more bottles of beer on the wall, no more bottles of beer."),
+            n => format!(
+                "{0} {1} of beer on the wall, {0} {1} of beer.",
+                n,
+                bottles(n)
+            ),
+        },
+        match n {
+            0 => String::from("Go to the store and buy some more, 99 bottles of beer on the wall."),
+            1 => String::from(
+                "Take it down and pass it around, no more bottles of beer on the wall."
+            ),
+            n => format!(
+                "Take one down and pass it around, {} {} of beer on the wall.",
+                n - 1,
+                bottles(n - 1)
+            ),
+        }
+    )
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    (end..=start)
+        .rev()
+        .map(verse)
+        .collect::<Vec<String>>()
+        .join("\n")
 }
--- exercises/practice/luhn/src/lib.rs
+++ exercises/practice/luhn/src/lib.rs
@@ -1,4 +1,12 @@
-/// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    code.chars()
+        .rev()
+        .filter(|c| !c.is_whitespace())
+        .try_fold((0, 0), |(sum, cnt), c| {
+            c.to_digit(10)
+                .map(|n| if cnt % 2 == 1 { n * 2 } else { n })
+                .map(|n| if n > 9 { n - 9 } else { n })
+                .map(|n| (sum + n, cnt + 1))
+        })
+        .map_or(false, |(sum, cnt)| cnt > 1 && sum % 10 == 0)
 }
